d1456c0529bd3dff0cca8d0449d8392d0f4b8979
Ian Pollack <ianjpollack@gmail.com>
2018-02-02 16:28:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,10 @@ function updatePlaylist(playlist, Artist, Song) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName];
+=======
+  delete playlist.artistName;
+>>>>>>> 0cbe5a8f0893c4d02f41eaea33a7a298c35f8de1
   return playlist
 }
